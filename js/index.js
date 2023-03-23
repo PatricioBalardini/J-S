41f7b218f5186c9a77ab7018e2b6c8e6c40d27ef
Patricio Balardini <patriciobalardini@gmail.com>
2023-03-23 18:22:20 +0100

    fixing category and route filtering

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 import { getData } from "./data.js";
 
 const cardsContainer = document.getElementById("cards");
-
+const checkboxContainer = document.getElementById("checkbox-container");
 const filterForm = document.getElementById("form-filters");
+const checkboxesAll = checkboxContainer.querySelectorAll(
+  'input[type="checkbox"]'
+);
+const searchBar = document.getElementById("searcher");
 
 let { currentDate, events } = await getData();
 
@@ -12,13 +16,14 @@ function getFormData(form) {
   return Object.fromEntries(formData);
 }
 
-filterForm.addEventListener("submit", function (e) {
+const handleFilterChange = (e) => {
   e.preventDefault();
-  const formData = getFormData(e.target);
+  const formData = getFormData(filterForm);
   const selectedCategories = Object.keys(formData).filter(
     (key) => key !== "searcher"
   );
   const searchValue = formData.searcher.toLowerCase().trim();
+
   const cardsFilter = events.filter((event) => {
     const isCategoryInclude = selectedCategories.length
       ? selectedCategories.includes(event.category)
@@ -26,12 +31,30 @@ filterForm.addEventListener("submit", function (e) {
     return event.name.toLowerCase().includes(searchValue) && isCategoryInclude;
   });
   createCard(cardsFilter);
+};
+
+filterForm.addEventListener("submit", function (e) {
+  handleFilterChange(e);
 });
-function createCard(arrayEvents) {
+
+searchBar.addEventListener("input", function (e) {
+  handleFilterChange(e);
+});
+
+checkboxesAll.forEach((checkbok) => {
+  checkbok.addEventListener("change", function (e) {
+    handleFilterChange(e);
+  });
+});
+
+function createCard(cardsFiltered) {
   cardsContainer.innerHTML = "";
-  let cards = "";
-  for (const event of arrayEvents) {
-    cards += `<div class="card" style="width: 240px">
+  let content = "";
+  if (!cardsFiltered.length) {
+    content = "Evento no encontrado";
+  } else {
+    for (const event of cardsFiltered) {
+      content += `<div class="card" style="width: 240px">
                    <img src="${event.image}" class="card-img-top" alt="..." />
                     <div class="card-body">
                       <h5 class="card-title">${event.name}</h5>
@@ -40,19 +63,9 @@ function createCard(arrayEvents) {
                       <a href="./pages/details.html?id=${event._id}" class="btn btn-primary">More Info</a>
                     </div>
                  </div> `;
+    }
   }
-  cardsContainer.innerHTML = cards;
+  cardsContainer.innerHTML = content;
 }
 
 createCard(events);
-
-// if (selectedCategory.length) {
-//   isCategoryIncluded = selectedCategories.includes(event.category);
-// } else {
-//   isCategoryIncluded = true;
-// }
-
-//   FormData(): permiten compilar un conjunto de pares clave/valor para enviar mediante XMLHttpRequest.
-//   Object.fromEntries(): transforma una lista de pares con [clave-valor] en un objeto.
-//   Object.keys(): devuelve un arreglo de propiedades enumerables propias de un objeto dado.
-//   Event.target: es una referencia al objeto en el cual se lanzo el evento.
